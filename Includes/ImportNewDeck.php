2c769dde3941223e20e26b0f2f9c8ea96afba9db
Bryan Bloomquist <bmbloomquist@gmail.com>
2025-01-15 09:36:09 -0600

    update all button works now

--- Includes/ImportNewDeck.php
+++ Includes/ImportNewDeck.php
@@ -8,6 +8,7 @@ namespace ArchidektImporter\Includes;
  */
 class ImportNewDeck
 {
+
     /**
      * Create a new Admin page for adding a new deck
      */
@@ -32,11 +33,17 @@ class ImportNewDeck
 ?>
         <div class="wrap">
             <h2>Add/Update Deck</h2>
+            <h3>Add A New Deck</h3>
             <p>Enter deck ID from Archidekt to add a new deck or to update an existing deck.</p>
             <form action="" method="post">
                 <label for="deck-id">Deck ID</label>
                 <input type="text" name="deck-id" id="deck-id" class="regular-text">
                 <input type="submit" name="fetch-deck-data" id="fetch-deck-data" class="button button-primary" value="Fetch Deck Data">
+            </form>
+            <h3>Update All Decks</h3>
+            <p>Pull all decks data from Archidekt to update table.</p>
+            <form action="" method="post">
+                <input type="submit" name="fetch-all-decks-data" id="fetch-all-decks-data" class="button button-primary" value="Update All Decks">
             </form>
         </div>
 <?php
@@ -50,17 +57,17 @@ class ImportNewDeck
         if (isset($_POST['fetch-deck-data'])) {
             $deck_id = sanitize_text_field($_POST['deck-id']);
 
-            $deck_id_exists = get_posts(array(
+            $deck_post = get_posts(array(
                 'post_type' => 'deck',
-                'meta_query' => array(['key' => 'archidekt_deck_id', 'value' => $deck_id]),
+                'meta_query' => array(['key' => 'deck_id', 'value' => $deck_id]),
             ));
 
             $deck_data = self::import_deck_data($deck_id);
 
-            if ($deck_id_exists) {
-                echo '<div class="notice notice-error is-dismissible"><p>Deck ID already exists. Please enter a unique Deck ID.</p></div>';
-
-                self::check_for_updates($deck_id, $deck_data);
+            if ($deck_post) {
+                $deck_post_id = $deck_post[0]->ID;
+                echo '<div class="notice notice-success is-dismissible"><p>Deck "' . $deck_data['name'] . '" data has been updated successfully.</p></div>';
+                self::update_deck_post_meta($deck_id, $deck_data, $deck_post_id);
             } else {
                 if ($deck_data) {
                     self::create_new_deck_post($deck_id, $deck_data);
@@ -71,6 +78,38 @@ class ImportNewDeck
             }
         }
     }
+
+    /**
+     * Trigger to update all deck data
+     */
+    public static function update_all_deck_button_trigger()
+    {
+        if (isset($_POST['fetch-all-decks-data'])) {
+            $deck_posts = get_posts(array(
+                'post_type' => 'deck',
+                'posts_per_page' => -1
+            ));
+
+            foreach ($deck_posts as $deck) {
+                $deck_post_id = $deck->ID;
+                $deck_id = get_post_meta($deck_post_id, 'deck_id', true);
+                if ($deck_id) {
+                    $deck_data = self::import_deck_data($deck_id);
+                    if ($deck_data) {
+                        self::update_deck_post_meta($deck_id, $deck_data, $deck_post_id);
+                        echo '<div class="notice notice-success is-dismissible"><p>Deck "' . $deck_data['name'] . '" data has been updated successfully.</p></div>';
+                    } else {
+                        echo '<div class="notice notice-error is-dismissible"><p>The deck_data for "' . $deck->post_title . '" could not be found.</p></div>';
+                    }
+                } else {
+                    echo '<div class="notice notice-error is-dismissible"><p>The deck_id for "' . $deck->post_title . '" could not be found.</p></div>';
+                }
+            }
+            // echo '<div class="notice notice-success is-dismissible"><p>All decks data has been updated successfully.</p></div>';
+        }
+    }
+
+
 
     /**
      * Fetch deck data from Archidekt API
@@ -102,14 +141,29 @@ class ImportNewDeck
      */
     public static function create_new_deck_post($deck_id, $deck_data)
     {
+        $deck_post_id = wp_insert_post(array(
+            'post_title' => $deck_data['name'],
+            'post_type' => 'deck',
+            'post_status' => 'publish'
+        ));
+
+        self::update_deck_post_meta($deck_id, $deck_data, $deck_post_id);
+    }
+
+    /**
+     * Update Deck Post Meta
+     */
+    public static function update_deck_post_meta($deck_id, $deck_data, $deck_post_id)
+    {
         $sorted_deck    = ProcessIncomingDeck::sort_cards_by_type($deck_data);
+        $deck_name      = $deck_data['name'];
         $deck_url       = "https://archidekt.com/decks/{$deck_id}";
         $commander      = $sorted_deck['Commander'][0]['card']['oracleCard']['name'];
         $partner        = $sorted_deck['Commander'][1]['card']['oracleCard']['name'];
         $identity       = ProcessIncomingDeck::get_color_identity($sorted_deck['Commander']);
         $misc_values    = ProcessIncomingDeck::calculate_misc_values($deck_data);
         $salt_sum       = $misc_values['salt_sum'];
-        $deck_price     = number_format($misc_values['price'], 2);
+        $deck_price     = "$" . number_format($misc_values['price'], 2);
         $total_mana     = $misc_values['mana_value'];
         $battles        = ProcessIncomingDeck::count_card_type($sorted_deck['Battle']);
         $planeswalkers  = ProcessIncomingDeck::count_card_type($sorted_deck['Planeswalker']);
@@ -122,12 +176,7 @@ class ImportNewDeck
         $non_lands      = ($partner ? 98 : 99) - $lands;
         $average_mana   = number_format($total_mana / $non_lands, 2);
 
-        $deck_post_id = wp_insert_post(array(
-            'post_title' => $deck_data['name'],
-            'post_type' => 'deck',
-            'post_status' => 'publish'
-        ));
-
+        wp_update_post(['ID' => $deck_post_id, 'post_title' => $deck_name]);
         update_post_meta($deck_post_id, 'deck_id', $deck_id);
         update_post_meta($deck_post_id, 'deck_name', $deck_data['name']);
         update_post_meta($deck_post_id, 'deck_data', $deck_data);
@@ -148,30 +197,8 @@ class ImportNewDeck
         update_post_meta($deck_post_id, 'enchantments', $enchantments);
         update_post_meta($deck_post_id, 'lands', $lands);
     }
-
-    /**
-     * Update existing Deck post if Deck ID already exists
-     */
-    public static function check_for_updates($deck_id, $deck_data)
-    {
-        $deck_post = get_posts(array(
-            'post_type' => 'deck',
-            'meta_query' => array(['key' => 'archidekt_deck_id', 'value' => $deck_id]),
-        ));
-        $deck_post_id = $deck_post[0]->ID;
-        $existing_deck_data = get_post_meta($deck_post_id, 'archidekt_deck_data', true);
-        if ($deck_data['updatedAt'] > $existing_deck_data['updatedAt']) {
-            update_post_meta($deck_post_id, 'archidekt_deck_data', $deck_data);
-            if ($deck_data['name'] !== $existing_deck_data['name']) {
-                wp_update_post(array(
-                    'ID' => $deck_post_id,
-                    'post_title' => $deck_data['name']
-                ));
-            }
-            echo '<div class="notice notice-success is-dismissible"><p>Deck data updated successfully.</p></div>';
-        }
-    }
 }
 
 add_action('admin_menu', ['ArchidektImporter\Includes\ImportNewDeck', 'add_new_deck_page']);
 add_action('admin_init', ['ArchidektImporter\Includes\ImportNewDeck', 'check_for_unique_id']);
+add_action('admin_init', ['ArchidektImporter\Includes\ImportNewDeck', 'update_all_deck_button_trigger']);
